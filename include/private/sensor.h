e61660d708d095674afcfd622b014e1fddace5ac
jennaluz <jpura827@gmail.com>
2023-03-18 17:15:28 -0700

    Remove sensor_base_e

--- include/private/sensor.h
+++ include/private/sensor.h
@@ -13,10 +13,5 @@
 extern QueueHandle_t temperature_queue;
 extern QueueHandle_t humidity_queue;
 
-typedef enum sensor_base_e {
-    DECIMAL     = 0,
-    HEXADECIMAL = 1,
-} sensor_base_e;
-
 
 void sensor_handler();
